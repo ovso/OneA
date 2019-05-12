dd5537649a2c97e215ddfc45fbceb9e72410ff47
jaeho <ovsoce@gmail.com>
2019-05-12 20:21:42 +0900

    BR programmatically

--- app/src/main/java/io/github/ovso/onea/App.java
+++ app/src/main/java/io/github/ovso/onea/App.java
@@ -1,9 +1,14 @@
 package io.github.ovso.onea;
 
 import android.app.Application;
+import android.content.ContextWrapper;
+import android.content.IntentFilter;
 import com.facebook.stetho.Stetho;
+import com.pixplicity.easyprefs.library.Prefs;
 import io.github.ovso.onea.data.db.AppDatabase;
 import io.github.ovso.onea.data.rx.RxBus;
+import io.github.ovso.onea.receiver.TestReceiver;
+import io.github.ovso.onea.utils.Consts;
 import lombok.Getter;
 import timber.log.Timber;
 
@@ -18,8 +23,30 @@ public class App extends Application {
     rxBus = new RxBus();
     appDb = AppDatabase.getInstance(this);
     Stetho.initializeWithDefaults(this);
+    setupTimber();
+    setupPrefs();
+    registerReceiver();
+  }
+
+  private void registerReceiver() {
+    IntentFilter intentFilter = new IntentFilter();
+    intentFilter.addCategory("android.intent.category.DEFAULT");
+    intentFilter.addAction(Consts.BR_ACTION_NAME_A);
+    registerReceiver(new TestReceiver(), intentFilter);
+  }
+
+  private void setupTimber() {
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
     }
+  }
+
+  private void setupPrefs() {
+    new Prefs.Builder()
+        .setContext(this)
+        .setMode(ContextWrapper.MODE_PRIVATE)
+        .setPrefsName(getPackageName())
+        .setUseDefaultSharedPreference(true)
+        .build();
   }
 }
